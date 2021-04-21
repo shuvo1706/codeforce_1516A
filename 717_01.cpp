17895c477831ddd45d97456ae5bd5c5a144c85df
shuvo1706 <buet1706@gmail.com>
2021-04-21 22:19:48 +0600

    Add files via upload

---
+++ 717_01.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n,k;
+    cin>>t;
+    while (t--)
+    {
+        cin>>n>>k;
+        int a[n];
+        for (int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+
+            for (int i=0;i<n-1;i++)
+            {
+                if (k==0)
+                    break;
+                if (a[i]==0)
+                    continue;
+                else
+                {
+                  if (k-a[i]>=0)
+                  {
+                      k=k-a[i];
+                     a[n-1]=a[n-1]+a[i];
+                      a[i]=0;
+
+
+                  }
+                    else
+                    {
+                        a[i]=a[i]-k;
+                         a[n-1]=a[n-1]+k;
+                        k=0;
+
+
+
+
+                    }
+
+
+
+                }
+            }
+
+            for (int i=0;i<n;i++)
+            {
+                                cout<<a[i]<<" ";
+
+            }
+    cout<<endl;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
