350915a0508e990f279c09cbce3c6a470cb03a2b
Shuvo Chakma <shuvo20005@gmail.com>
2021-04-25 11:16:51 +0600

    Add files via upload

---
+++ atc199_01.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if (c*c>a*a+b*b)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+
+    return 0;
+}
