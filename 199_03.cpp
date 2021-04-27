8c732b9b69155e06bc86015ef899c1b3442b0d18
Shuvo Chakma <shuvo20005@gmail.com>
2021-04-27 17:38:38 +0600

    Add files via upload

---
+++ 199_03.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t,a,b,q,l,flag=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    l=s.length();
+    cin>>q;
+    for (int i=0;i<q;i++)
+    {
+        cin>>t>>a>>b;
+        if (t==2)
+
+            flag^=1;
+            else
+            {
+                if (!flag)
+                    swap(s[a-1],s[b-1]);
+                else
+                {
+                      if (a<=n)
+                        a+=n;
+                      else if (a>n)
+                        a-=n;
+                        if (b<=n)
+                        b+=n;
+                      else if (b>n)
+                        b-=n;
+                         swap(s[a-1],s[b-1]);
+                }
+
+            }
+    }
+   if (flag)
+            {
+                s=s.substr(n,n)+s.substr(0,n);
+                cout<<s<<endl;
+            }
+            else
+                cout<<s<<endl;
+    return 0;
+}
