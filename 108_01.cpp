f7c657a656cb0dd8a44f4d7c34bf989685a49b28
Shuvo Chakma <shuvo20005@gmail.com>
2021-04-30 00:16:35 +0600

    Add files via upload

---
+++ 108_01.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+long long int r,b,d;
+while(t--)
+{
+    cin>>r>>b>>d;
+    if (abs(r-b)<=d)
+        cout<<"yes"<<endl;
+    else
+    {
+        long long int mini=(r<b)?r:b;
+
+        long long int maxi=(r>b)?r:b;
+
+        if (((d+1)*mini)>=maxi)
+        {
+            cout<<"yes"<<endl;
+
+        }
+        else
+               cout<<"no"<<endl;
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
