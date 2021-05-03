6f87f54b40df77007891fe293650afe593668c5f
Shuvo Chakma <shuvo20005@gmail.com>
2021-05-03 13:24:08 +0600

    Add files via upload

---
+++ 4_3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int n,j=1;
+    cin >> n;
+
+    map<string, int> database;
+    string s;
+    while (n--)
+    {
+        cin >> s;
+        if (database[s] == 0)
+        {
+            cout << "OK" << endl;
+            database[s] = 1;
+
+        }
+        else
+        {
+                               database[s]=database[s]+1;
+                               s=s+to_string(database[s]-1);
+
+                            cout<<s<<endl;
+                                database[s]=database[s]+1;
+
+        }
+    }
+    return 0;
+}
